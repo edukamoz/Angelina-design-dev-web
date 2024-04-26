8f51e7e396db05937ceedd3afa18709325ff681e
Eduardo Fatec <eduardokamoz@gmail.com>
2024-04-26 19:49:49 -0300

    Comitado

---
+++ hamburgueria/script.js
@@ -0,0 +1,21 @@
+let adicionais, entrega = 0
+
+function totalPagar() {
+
+    let total = 0
+    calcularAdicionais()
+
+}
+
+function calcularAdicionais() {
+
+    let selecionados = document.querySelectorAll("input[name='adicionais']")
+    let qnt = selecionados.length
+
+}
+
+function calcularEntrega() {
+
+
+
+}
